820047d7f7086129e3fe6b70be14e4774c90e67e
Adriano Nascimento <ns.adriano@gmail.com>
2021-03-25 20:19:42 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Position position = new Position(3, 4);
+		System.out.print(position);
 
 	}
 
