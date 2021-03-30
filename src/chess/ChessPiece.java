0f46aa4b70c058edef52bd61cc33969cf9ce1741
Adriano Nascimento <ns.adriano@gmail.com>
2021-03-30 11:20:18 -0300

    ChessException and ChessPosition

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -5,7 +5,7 @@ import boardgame.Piece;
 
 public class ChessPiece extends Piece{
 	private Color color;
-	private int moveCount;
+	//private int moveCount;
 	
 	public ChessPiece(Board board, Color color) {
 		super(board);
